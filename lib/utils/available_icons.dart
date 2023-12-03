7f23d3390015a783bbc7507110c36348c73e9b4c
Salvist <richiechandra47@yahoo.com>
2023-12-03 14:34:59 -0500

    added more icons

--- lib/utils/available_icons.dart
+++ lib/utils/available_icons.dart
@@ -8,9 +8,21 @@ const categoryIcons = <String, IconData>{
   'emoji_food_beverage': Icons.emoji_food_beverage_rounded,
   'local_bar': Icons.local_bar_rounded,
   'liquor': Icons.liquor_rounded,
+  'shopping_cart': Icons.shopping_cart_rounded,
 
+  // Beayty
   'checkroom': Icons.checkroom_rounded,
   'local_laundry_service': Icons.local_laundry_service_rounded,
+
+  // Hobby
+  'florist': Icons.local_florist_rounded,
+  'photo_camera': Icons.photo_camera_rounded,
+  'soccer': Icons.sports_soccer_rounded,
+  'basketball': Icons.sports_basketball_rounded,
+  'esports': Icons.sports_esports_rounded,
+  'television': Icons.tv_rounded,
+  'chair': Icons.chair_rounded,
+  'couch': Icons.weekend_rounded,
 
   // Transportations
   'directions_bike': Icons.directions_bike_rounded,
@@ -23,13 +35,15 @@ const categoryIcons = <String, IconData>{
   'flight': Icons.flight_rounded,
   'directions': Icons.directions_boat_rounded,
 
-  // Gas
+  // Utilities
   'local_gas_station': Icons.local_gas_station_rounded,
   'propane_tank': Icons.propane_tank_rounded,
   'ev_station': Icons.ev_station_rounded,
-
-  // Electricity
   'bolt': Icons.bolt_rounded,
+  'electric_bolt': Icons.electric_bolt_rounded,
+
+  'house': Icons.house_rounded,
+  'apartment': Icons.apartment_rounded,
 
   'question_mark': Icons.question_mark_rounded,
 };
